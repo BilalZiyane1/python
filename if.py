36e24689a78de4361d3d0bff05f1198e6718a471
bilalziyane1 <bilal.ziyane@usmba.ac.ma>
2024-04-21 19:19:45 +0100

    first commit

---
+++ if.py
@@ -0,0 +1,23 @@
+# age=int(input("how old are you? "))
+# if age== 100:
+#     print("you're a century old!")
+# elif age >=18 :
+#     print("you're an adult!")
+# elif age <0:
+#     print("you haven't been born yet!")
+# else:
+#     print("you're a chid!") 
+
+age = int(input("what's your current age ?\n"))
+if age <=10:
+    print("you still a little child")
+elif age>10 and age <18:
+    print("you ain't adult yet ")
+else:
+    print("you re an adult!");
+    
+food= ["bimo","danon","cheesecake","me"]
+for i in food:
+    p=1
+    print("food number"+ p)
+    p=p+1
